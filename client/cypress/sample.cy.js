5f910ebaaf1cf35da8beae9752777f57b1967dc4
kanew <kaniujeffray@gmail.com>
2025-07-21 20:32:23 +0300

    Deleted all files

--- client/cypress/sample.cy.js
+++
@@ -1,7 +0,0 @@
-describe('MERN App E2E', () => {
-  it('should display the heading and button', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Welcome to the MERN Testing App');
-    cy.contains('Click Me');
-  });
-}); 
